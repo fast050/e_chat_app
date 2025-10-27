15c08534a470293e430dde6093f25568d6e8bdcd
Khalid <khalidwork050@gmail.com>
2025-10-27 15:36:24 +0400

    create project structure files

---
+++ lib/features/e_chat_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class EChatApp extends StatelessWidget{
+
+  const EChatApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+     return MaterialApp(
+        home: Text("Home Page"),
+        theme: ThemeData(),
+     );
+  }
+
+}
